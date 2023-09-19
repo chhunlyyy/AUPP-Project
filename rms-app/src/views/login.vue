8fbde727011b0588a77017243e330b6648fdc5bc
Chiva <tepchiva018@gmail.com>
2023-09-19 19:22:58 +0700

    hot fix

--- rms-app/src/views/login.vue
+++ rms-app/src/views/login.vue
@@ -27,6 +27,7 @@
               class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
           </div>
         </div>
+        <p v-if="isShowError">Failed login</p>
         <div class="mt-5">
           <button @click="handleLogin()" :disabled="email == null || password == null" class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Signin</button>
         </div>
@@ -39,15 +40,19 @@
   import { ref } from 'vue'
   import { useRouter, useRoute } from 'vue-router';
 
-  const { login } = useSessionLogin();
+  const { login, isAuthenticated } = useSessionLogin();
 
   const email = ref(null);
   const password = ref(null);
   const router = useRouter();
   const route = useRoute();
+  const isShowError = ref(false);
 
   const handleLogin = async () => {
     await login(email.value, password.value);
-    router.push(route.redirectedFrom ? route.redirectedFrom.fullPath : "/").then(() => router.go());
+    if(!isAuthenticated.value)
+      isShowError.value = true;
+    else
+      router.push(route.redirectedFrom ? route.redirectedFrom.fullPath : "/").then(() => router.go());
   }
 </script>
