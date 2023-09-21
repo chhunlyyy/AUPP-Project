373303e63af67cf1d24c541395145972ba68ef2f
Chiva <tepchiva018@gmail.com>
2023-09-21 20:24:43 +0700

    add prime vue design

--- rms-app/src/main.js
+++ rms-app/src/main.js
@@ -1,10 +1,14 @@
-import './assets/tailwind.css'
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import './assets/tailwind.css';
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+import PrimeVue from "primevue/config";
+import Tailwind from "primevue/passthrough/tailwind";
+
 
 const app = createApp(App)
 
 app.use(router)
+app.use(PrimeVue, { unstyled: true, pt: Tailwind });
 
 app.mount('#app')
